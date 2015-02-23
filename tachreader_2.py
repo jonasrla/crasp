15b096a2662b7ca35e502d57d19b0c978e981a7c
Jonas Rocha Lima Amaro <jonasrla@gmail.com>
2015-02-23 17:10:02 -0500

    add tachreaders
    adds first stable version of tachometer reader and a alternative version

--- tachreader_2.py
+++ tachreader_2.py
@@ -13,28 +13,23 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(right_port, GPIO.IN)
 GPIO.setup(left_port, GPIO.IN)
 
-counter_left = 0
-counter_right = 0
-begin_time_left = 0
-begin_time_right = 0
-end_time_left = 0
-end_time_right = 0
+left_f = []
+right_f = []
 # system_f = 0
 
 lock = threading.Lock()
 
 def right_tach():
-    global counter_right
-    global begin_time_right
-    global end_time_right
-    begin_time_right = time()
+    global right_f
+    temp1 = time()
     # counter = 0
     # avg = []
     while True:
         lock.acquire()
         GPIO.wait_for_edge(right_port, GPIO.BOTH)
-        end_time_right = time()
-        counter_right += 1
+        temp2 = time()
+        right_f.append(pi/(18*(temp2 - temp1)))
+        temp1 = temp2
         lock.release()
         # counter += 1
         # avg.append(right_f)
@@ -45,17 +40,16 @@ def right_tach():
 
 
 def left_tach():
-    global counter_left
-    global begin_time_left
-    global end_time_left
-    begin_time_left = time()
+    global left_f
+    temp1 = time()
     # counter = 0
     # avg = []
     while True:
         lock.acquire()
         GPIO.wait_for_edge(left_port, GPIO.BOTH)
-        end_time_left = time()
-        counter_left += 1
+        temp2 = time()
+        left_f.append(pi/(18*(temp2 - temp1)))
+        temp1 = temp2
         lock.release()
         # counter += 1
         # avg.append(left_f)
@@ -65,31 +59,27 @@ def left_tach():
         #   counter = 0
 
 def read_left():
-    global counter_left
-    global begin_time_left
-    global end_time_left
-    if counter_left:
-        lock.acquire()
-        ave = (pi*counter_left)/(9*(end_time_left-begin_time_left))
-        begin_time_left = end_time_left
-        counter_left = 0
-        lock.release()
+    global left_f
+    lock.acquire()
+    if len(left_f):
+        ave = sum(left_f)/len(left_f)
+        print 'left len:', len(left_f)
+        left_f = []
     else:
         ave = 0
+    lock.release()
     return ave
 
 def read_right():
-    global counter_right
-    global begin_time_right
-    global end_time_right
-    if counter_right:
-        lock.acquire()
-        ave = (pi*counter_right)/(9*(end_time_right-begin_time_right))
-        begin_time_right = end_time_right
-        counter_right = 0
-        lock.release()
+    global right_f
+    lock.acquire()
+    if len(right_f):
+        ave = sum(right_f)/len(right_f)
+        print 'right len:', len(right_f)
+        right_f = []
     else:
         ave = 0
+    lock.release()
     return ave
 
 # def sys_tach():
